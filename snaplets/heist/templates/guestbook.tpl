357f1a2e1a99bf986e74722e69c08d28e374de73
Jacek Lehmann <vonschlager@wp.pl>
2013-06-29 08:07:13 +0200

    guestbook

---
+++ snaplets/heist/templates/guestbook.tpl
@@ -0,0 +1,15 @@
+<apply template="index">
+  <guestbook>
+    <div>
+      <h5><nick/></h5>
+      <h5><email/></h5>
+      <h5><www/></h5>
+      <div>
+        <body/>
+      </div>
+      <p class="text-right">
+        <small><creation/></small>
+      </p>
+    </div>
+  </guestbook>
+</apply>
---
+++ src/Handlers/Guestbook.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Handlers.Guestbook
+    ( handleGuestbook
+    ) where
+
+import Data.Maybe (fromMaybe)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Heist.Interpreted
+import Snap.Core
+import Snap.Snaplet
+import Snap.Snaplet.Heist
+import qualified Text.XmlHtml as X
+
+import Application
+import Db
+import Utils
+
+renderGuestbook :: DbGuestbook -> Splice (Handler App App)
+renderGuestbook g = runChildrenWith
+    [ ("id", textSplice . showAsText $ fromMaybe 0 $ gId g)
+    , ("nick", textSplice $ gNick g)
+    , ("email", textSplice $ gEmail g)
+    , ("www", textSplice $ gWww g)
+    , ("body", nodes $ gBody g)
+    , ("creation", textSplice . showAsText $ gCreation g)
+    ]
+  where nodes t = do case X.parseHTML (T.unpack $ gNick g) $ T.encodeUtf8 t of
+                      Left err -> return [X.TextNode $ T.pack err]
+                      Right d  -> return $ X.docContent d
+
+handleGuestbook :: Handler App App ()
+handleGuestbook = do
+    guestbook <- with db $ getGuestbook
+    heistLocal (splices guestbook) $ render "/guestbook"
+  where
+    splices gs = bindSplices [("guestbook", mapSplices renderGuestbook gs)]
